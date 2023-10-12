5a8ba25df48039d286383d40e45e31e770bda773
Densho <denj.rey@gmail.com>
2023-10-12 12:03:34 -0400

    Products API Initial Work

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to DressStore application.' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://dreyes18:Newy0rkcity!@dressstore.omjewvf.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const productRoutes = require('./routes/productRoutes');
+
+app.use(productRoutes);
