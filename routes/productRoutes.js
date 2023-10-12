0c854b7509240b7439101c8ef9eb5fb59df11d68
Densho <denj.rey@gmail.com>
2023-10-12 16:23:27 -0400

    Moved fuctionality to routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,127 +1,18 @@
 const express = require('express');
-const { getAllProducts } = require('../controllers/productController');
+const productController = require('../controllers/productController');
 const Product = require('../models/Product');
 
 const router = express.Router();
 
-router.get('/api/products', async (req, res) => {
-    var keyword = req.query.name;
+router.get('/api/products', productController.getAllProducts);
+router.get('/api/products/published', productController.getPublishedProducts);
+router.get('/api/products/:id', productController.getProductById);
 
-    if (keyword == null) {
-        getAllProducts(req, res);
-    }
-    else {
-        try {
-            keyword = keyword.replace('[','').replace(']','').trim();
-            const products = await Product.find({
-                name: { $regex: keyword, $options: 'i' }
-            });
-            
-            res.status(200).json(products);
-        }
-        catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'Server error while fetching products.' });
-        }
-    }
-});
+router.post('/api/products', productController.createProduct);
 
-router.get('/api/products', getAllProducts);
+router.put('/api/products/:id', productController.updateProduct);
 
-router.get('/api/products/published', async (req, res) => {
-    try {
-        const publishedProducts = await Product.find({ published: true });
-        res.status(200).json(publishedProducts);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error while fetching published products.' });
-    }
-});
-
-router.get('/api/products/:id', async (req, res) => {
-    const productId = req.params.id;
-
-    try {
-        const product = await Product.findById(productId);
-        
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found.' });
-        }
-
-        res.status(200).json(product);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error while fetching product.' });
-    }
-});
-
-router.post('/api/products', async (req, res) => {
-    const { name, description, price, published, category } = req.body;
-
-    if (!name || !description || price === undefined || published === undefined || !category) {
-        return res.status(400).json({ message: 'All fields are required.' });
-    }
-
-    try {
-        const newProduct = new Product({
-            name,
-            description,
-            price,
-            published,
-            category
-        });
-
-        const savedProduct = await newProduct.save();
-
-        res.status(201).json(savedProduct);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error while adding product.' });
-    }
-});
-
-router.put('/api/products/:id', async (req, res) => {
-    const productId = req.params.id;
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, { new: true, runValidators: true });
-        
-        if (!updatedProduct) {
-            return res.status(404).json({ message: 'Product not found.' });
-        }
-
-        res.status(200).json(updatedProduct);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error while updating product.' });
-    }
-});
-
-router.delete('/api/products/:id', async (req, res) => {
-    const productId = req.params.id;
-
-    try {
-        const product = await Product.findByIdAndDelete(productId);
-        
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found.' });
-        }
-
-        res.status(200).json({ message: 'Product successfully deleted.', deletedProduct: product });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error while deleting product.' });
-    }
-});
-
-router.delete('/api/products', async (req, res) => {
-    try {
-        const products = await Product.deleteMany({});
-        res.status(200).json({ message: 'All products successfully deleted.', deletedProducts: products });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error while deleting products.' });
-    }
-});
+router.delete('/api/products/:id', productController.deleteProductById);
+router.delete('/api/products', productController.deleteAllProducts);
 
 module.exports = router;
